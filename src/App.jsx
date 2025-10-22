96572953be95b6ec21bd56b253c06e51214dcced
Istheaksayem <istheakahmed9345@gmail.com>
2025-10-22 09:38:30 +0600

    setup react project with react router ,tailwind css,DaisyUi and navber

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+      <h1 className='text-2xl text-red-600'>Assignment 9 project</h1>
+      
+     
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/component/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {  Link, NavLink } from 'react-router'
+import logo from '../../assets/WhatsApp Image 2025-10-21 at 21.28.52_e3400c53.png'
+
+
+const NavBar = () => {
+    return (
+           <div className='flex items-center justify-between px-6 py-3 bg-gray-100'>
+      
+      {/* Left side - Logo */}
+      <div className="text-xl font-bold">
+      <img className='w-15 rounded-2xl' src={logo} alt="" />
+      </div>
+
+      {/* Right side - Home & Profile */}
+      <div className="flex items-center gap-6">
+        <NavLink to="/" >Home</NavLink>
+        <NavLink to="/profile" >My Profile</NavLink>
+        <Link className="btn btn-neutral">Login</Link>
+      </div>
+    </div>
+    );
+};
+
+export default NavBar;
