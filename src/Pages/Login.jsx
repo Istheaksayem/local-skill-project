1501da16bf45b334ed439f803faee53cb49516b6
Istheaksayem <istheakahmed9345@gmail.com>
2025-10-23 19:38:37 +0600

    added login  and register form

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className='flex justify-center min-h-screen items-center'>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+                <form className="card-body">
+                    <h2 className='text-2xl font-semibold text-center'>Login your account</h2>
+                    <fieldset className="fieldset">
+                        {/* email */}
+                        <label className="label">Email</label>
+                        <input 
+                        type="email" 
+                        name='email' 
+                        className="input" 
+                        placeholder="Email" 
+                        required
+                        />
+                        {/* password  */}
+                        <label className="label">Password</label>
+                        <input 
+                        type="password" 
+                        name='password' 
+                        className="input" 
+                        placeholder="Password" 
+                        required
+                        />
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <button type='submit' className="btn btn-neutral mt-4">Login</button>
+                        <p className='text-center font-semibold'>Don’t Have An Account ? <Link className='text-secondary' to="/auth/register">Register</Link></p>
+                    </fieldset>
+                </form>
+            </div>
         </div>
     );
 };
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router';
+
 
 const Register = () => {
     return (
-        <div>
-            
+        <div className='flex justify-center min-h-screen items-center'>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+                <form className="card-body">
+                    <h2 className='text-2xl font-semibold text-center'>Register your account</h2>
+                    <fieldset className="fieldset">
+                        {/* User name  */}
+                        <label className="label">User Name</label>
+                        <input
+                            type="text"
+                            name='name'
+                            className="input"
+                            placeholder="Your name"
+                            required
+                        />
+                        {/* Photo url  */}
+                        <label className="label">Photo Url</label>
+                        <input
+                            type="text"
+                            name='photo'
+                            className="input"
+                            placeholder="Photo url"
+                            required
+                        />
+                        {/* email */}
+                        <label className="label">Email</label>
+                        <input
+                            type="email"
+                            name='email'
+                            className="input"
+                            placeholder="Email"
+                            required
+                        />
+                        {/* password  */}
+                        <label className="label">Password</label>
+                        <input
+                            type="password"
+                            name='password'
+                            className="input"
+                            placeholder="Password"
+                            required
+                        />
+
+                        <button type='submit' className="btn btn-neutral mt-4">Register</button>
+                        <p className='text-center font-semibold'>Don’t Have An Account ? <Link className='text-secondary' to="/auth/login">Login</Link></p>
+                    </fieldset>
+                </form>
+            </div>
         </div>
     );
 };
